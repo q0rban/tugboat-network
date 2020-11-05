c00ca303f91f26c043a37a39ae764e39f296df9c
James Sansbury <james@lullabot.com>
2020-11-05 16:08:21 -0500

    Fix indexes.

--- dark/index.php
+++ dark/index.php
@@ -1,13 +1,9 @@
 <?php require_once '../curl_request.php'; ?>
 <html>
-<body>
+  <body>
     <h1>Welcome to Dark Tugboat!</h1>
-    <pre>
-      <?php print file_get_contents('light.txt') ?>
-    </pre>
+    <pre><?php print file_get_contents('dark.txt') ?></pre>
     <h1>This is the Light Tugboat logo pulled from the other service using cURL:</h1>
-    <pre>
-      <?php print _curl_request('http://light/light.txt') ?>
-    </pre>
-</body>
+    <pre><?php print _curl_request('http://light/light.txt') ?></pre>
+  </body>
 </html>
--- light/index.php
+++ light/index.php
@@ -1,13 +1,9 @@
 <?php require_once '../curl_request.php'; ?>
 <html>
-<body>
-<h1>Welcome to Light Tugboat!</h1>
-<pre>
-      <?php print file_get_contents('light.txt') ?>
-    </pre>
-<h1>This is the Dark Tugboat logo pulled from the other service using cURL:</h1>
-<pre>
-      <?php print _curl_request('http://dark/dark.txt') ?>
-    </pre>
-</body>
+  <body>
+    <h1>Welcome to Light Tugboat!</h1>
+    <pre><?php print file_get_contents('light.txt') ?></pre>
+    <h1>This is the Dark Tugboat logo pulled from the other service using cURL:</h1>
+    <pre><?php print _curl_request('http://dark/dark.txt') ?></pre>
+  </body>
 </html>
