2a00a0cee553e842fb700bc227544d241da1c4cc
James Sansbury <james@lullabot.com>
2020-11-05 16:57:58 -0500

    Use the public default service URL on the light service.

--- light/index.php
+++ light/index.php
@@ -4,6 +4,6 @@
     <h1>Welcome to Light Tugboat!</h1>
     <pre><?php print file_get_contents('light.txt') ?></pre>
     <h1>This is the Dark Tugboat logo pulled from the other service using cURL:</h1>
-    <pre><?php print _curl_request('http://dark/dark.txt') ?></pre>
+    <pre><?php print _curl_request(getenv('TUGBOAT_DEFAULT_SERVICE_URL') . '/dark.txt') ?></pre>
   </body>
 </html>
